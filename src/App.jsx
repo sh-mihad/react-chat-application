5d3799059cc23eb865808849f4af9fc9c390bc39
Sabbir <sh.mihad.cse@gmail.com>
2024-11-26 23:47:23 +0600

    basic routing done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Login from "./pages/Auth/Login"
+import Register from "./pages/Auth/Register"
 import Home from "./pages/Home"
 
 function App() {
   return (
-    <>
-    <Home/>
-    </>
+   <BrowserRouter>
+    <Routes>
+      <Route  path="/" element={<Home/>} />
+      <Route  path="/login" element={<Login/>} />
+      <Route  path="/register" element={<Register/>} />
+    </Routes>
+   </BrowserRouter>
     
   )
 }
